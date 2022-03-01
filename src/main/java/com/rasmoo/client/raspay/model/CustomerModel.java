819281a11b96a73f775d29755220a8fac1831e3c
Felipe Alves <felipe.alves.stefanini@elo.com.br>
2022-03-01 18:01:19 -0300

    Altera service

--- src/main/java/com/rasmoo/client/raspay/model/CustomerModel.java
+++ src/main/java/com/rasmoo/client/raspay/model/CustomerModel.java
@@ -7,9 +7,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/rasmoo/client/raspay/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/rasmoo/client/raspay/service/impl/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.rasmoo.client.raspay.service.impl;
 
 import com.rasmoo.client.raspay.dto.CustomerDto;
+import com.rasmoo.client.raspay.exception.BusinessException;
 import com.rasmoo.client.raspay.mapper.CustomerMapper;
 import com.rasmoo.client.raspay.model.CustomerModel;
 import com.rasmoo.client.raspay.repository.CustomerRepository;
 import com.rasmoo.client.raspay.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,19 +15,24 @@ import java.util.Optional;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    private final CustomerRepository customerRepository;
+
     @Autowired
-    private CustomerRepository customerRepository;
+    CustomerServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public CustomerModel create(CustomerDto customerDto) {
         Optional<CustomerModel> customerEmailOpt = customerRepository.findByEmail(customerDto.getEmail());
-        CustomerModel customer = CustomerMapper.fromDtoToModel(customerDto);
         if (customerEmailOpt.isPresent()) {
             Optional<CustomerModel> customerPhoneOpt = customerRepository.findByTelephone(customerDto.getTelephone());
             if (customerPhoneOpt.isPresent()) {
                 if (matchInformations(customerEmailOpt.get().getEmail(),
                         customerEmailOpt.get().getEmail())) {
                     customerDto.setId(customerEmailOpt.get().getId());
+                } else {
+                    throw new BusinessException("Telefone está em uso por outro usuário.", HttpStatus.BAD_REQUEST);
                 }
             }
         }
