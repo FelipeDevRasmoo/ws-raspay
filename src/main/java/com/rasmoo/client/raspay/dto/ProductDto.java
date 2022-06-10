36e3deb6bb19ab91d5481a1e4253f44157e67d9b
Felipe Alves <felipe.alves@email.com>
2022-06-10 18:50:25 -0300

    permite sigla produto salvar mais do que 10 caracteres

--- src/main/java/com/rasmoo/client/raspay/dto/ProductDto.java
+++ src/main/java/com/rasmoo/client/raspay/dto/ProductDto.java
@@ -21,7 +21,7 @@ public class ProductDto {
     private String id;
 
     @NotBlank(message = "acronym é obrigatório")
-    @Size(min = 6,max = 10)
+    @Size(min = 6,max = 20)
     private String acronym;
 
     @NotBlank(message = "name é obrigatório")
